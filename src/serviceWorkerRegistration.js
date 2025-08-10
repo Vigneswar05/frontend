7a0186ec55dd8aeab78374d226f668d65ab3befa
vigneswar <vigneswar.05k@gmail.com>
2025-08-11 01:05:28 +0530

    Your commit message here

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,7 +1,7 @@
 export function register() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js')
+            navigator.serviceWorker.register('https://vigneswar05.github.io/frontend/service-worker.js')
                 .then(reg => console.log('Service worker registered', reg))
                 .catch(err => console.error('Service worker error', err));
         });
